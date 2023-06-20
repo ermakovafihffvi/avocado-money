2aa91f12f94b821c5aecd0a92bcf0538e6023b15
ermakovafihffvi <veraermakova.fihffvi@gmail.com>
2023-06-20 11:51:10 +0300

    removed unused elements from admin

--- resources/js/components/basecomponents/AdminButtonsTable.js
+++ resources/js/components/basecomponents/AdminButtonsTable.js
@@ -8,22 +8,11 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
-import { NavLink } from 'react-router-dom';
-import Button from '@mui/material/Button';
 
-import ChangeSavingSumModal from './ChangeSavingSumModal';
 
 function AdminButtonsTable(props) {
 
     let savingsArr = props.expenses;
-
-    const [rowStrId, setRowStrID] = useState(0);
-    const [open, setOpen] = useState(false);
-    const updateOpenState = () => {
-        setOpen(false);
-    }
-    useEffect(()=>{
-    }, [])
 
     return (
         <>
@@ -33,7 +22,6 @@ function AdminButtonsTable(props) {
                         <TableRow>
                             <TableCell>Category</TableCell>
                             <TableCell>Sum</TableCell>
-                            <TableCell>Submit</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
@@ -49,37 +37,13 @@ function AdminButtonsTable(props) {
                                         {row[1].sum}
                                     </Typography>
                                 </TableCell>
-                                <TableCell className="saving_buttons_tc">
-
-                                    <div className="saving_buttons_wrapper">
-                                        <Button 
-                                            variant="contained" 
-                                            value="confirm" 
-                                            onClick={(e) => 
-                                                props.handleConfirm(row[0], e.target.parentNode.parentNode.parentNode.querySelector("#saving_sum").childNodes[0].innerHTML)}
-                                        >Confirm</Button>
-                                        <Button 
-                                            value="correct"
-                                            onClick={(e) => {setOpen(true); setRowStrID(row[0])}}
-                                        >Correct</Button>
-                                    </div>
-                                
-        
-                                </TableCell>
+                    
                             </TableRow>
                         ))}
 
                     </TableBody>
                 </Table>
             </TableContainer>
-
-            <ChangeSavingSumModal 
-                open={ open } 
-                setOpen={ setOpen }
-                updateOpenState={ updateOpenState } 
-                handleConfirm={ props.handleConfirm }
-                rowStrId={ rowStrId }
-            ></ChangeSavingSumModal>
         </>
     )
 }
